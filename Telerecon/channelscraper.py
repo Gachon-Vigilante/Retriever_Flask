8f8e0c957e761c7b5efa7b5586a3aaf5468c1afc
SapphireLeo <softchocopie@gmail.com>
2024-12-15 04:12:41 +0900

    Added API mode
    모듈의 전체적인 API화 진행.
    1. 각 패키지에 __init__.py 추가.
    2. python에 Flask를 설치.
    3. app.py에서 Flask를 이용해서 API 엔드포인트 구축.
    4. 홍보글 내용 추출, 텔레그램 채널 데이터 수집에 대한 API 구축 및 테스트 완료.
    5. 모듈의 구동 위치가 app.py로 바뀌면서 session 파일 역시 app.py의 디렉토리로 이동.

--- Telerecon/channelscraper.py
+++ Telerecon/channelscraper.py
@@ -1,11 +1,11 @@
 import os
 import asyncio
-import details as ds
+from . import details as ds
+import base64
 from telethon.sync import TelegramClient, types
 from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument
 import pandas as pd
 from colorama import Fore, Style
-import sanitizer
 
 api_id = ds.apiID
 api_hash = ds.apiHash
@@ -18,30 +18,34 @@ async def scrape_channel_content(channel_name):
             entity = await client.get_entity(channel_name)
             content = []
             post_count = 0
-            media_directory = sanitizer.sanitize_filename(f"Media/{channel_name}")
-
-            if not os.path.exists(media_directory):
-                os.makedirs(media_directory)
 
             async for post in client.iter_messages(entity):
                 post_count += 1
 
                 text = post.text or ""
-                media_file_path = None
+                media_base64 = None
 
                 # 미디어 파일 다운로드
                 if post.media:
                     if isinstance(post.media, (MessageMediaPhoto, MessageMediaDocument)):
                         try:
-                            media_file_path = await client.download_media(
+                            media_bytes = await client.download_media(
                                 message=post,
-                                file=media_directory
-                            ) # media를 다운로드하는 코드. 만약 파일을 메모리의 바이트 객체로 저장하고 싶다면 client.download_media(post, file=bytes)를 사용하면 됨.
-                            print(f"{Fore.GREEN}Downloaded media: {media_file_path}{Style.RESET_ALL}")
+                                file=bytes
+                            ) # media를 다운로드하는 코드해서 바이트 객체로 저장하는 코드.
+                            print(f"{Fore.GREEN}Downloaded media.{Style.RESET_ALL}")
+                            # 다운로드한 media를 base64로 encode
+                            media_base64 = base64.b64encode(media_bytes).decode('utf-8')
                         except Exception as e:
                             print(f"{Fore.RED}Failed to download media: {e}{Style.RESET_ALL}")
                 # 메시지 보낸 날짜 및 시간
                 message_date = post.date.strftime('%Y-%m-%d %H:%M:%S')  # 'YYYY-MM-DD HH:MM:SS' 포맷으로 저장
+
+                # 기본적으로 모두 값 없음으로 초기화
+                username = "N/A"
+                first_name = "N/A"
+                last_name = "N/A"
+                user_id = "N/A"
 
                 if sender := post.sender:
                     if isinstance(sender, types.User):
@@ -49,21 +53,21 @@ async def scrape_channel_content(channel_name):
                         first_name = sender.first_name or "N/A"
                         last_name = sender.last_name if sender.last_name else "N/A"
                         user_id = sender.id
-                    else:
-                        username = "N/A"
-                        first_name = "N/A"
-                        last_name = "N/A"
-                        user_id = "N/A"
-                else:
-                    username = "N/A"
-                    first_name = "N/A"
-                    last_name = "N/A"
-                    user_id = "N/A"
 
                 views = post.views or "N/A"
                 message_url = f"https://t.me/{channel_name}/{post.id}"
 
-                content.append((message_date, text, username, first_name, last_name, user_id, views, message_url, media_file_path))
+                content.append({
+                    "date": message_date,
+                    "text": text,
+                    "username": username,
+                    "first_name": first_name,
+                    "last_name": last_name,
+                    "user_id": user_id,
+                    "views": views,
+                    "url": message_url,
+                    "media": media_base64,
+                })
 
                 if post_count % 10 == 0:
                     print(
@@ -75,40 +79,46 @@ async def scrape_channel_content(channel_name):
             print(f"An error occurred: {Fore.RED}{e}{Style.RESET_ALL}")
             return []
 
+def main(channel_name):
+    # 비동기 데이터 수집 실행
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    content = loop.run_until_complete(scrape_channel_content(channel_name))
+    return content
 
-async def main():
-    try:
-        channel_name = input(
-            f"{Fore.CYAN}Please enter a target Telegram channel (e.g., https://t.me/{Fore.LIGHTYELLOW_EX}your_channel{Style.RESET_ALL}):\n")
-        print(f'You entered "{Fore.LIGHTYELLOW_EX}{channel_name}{Style.RESET_ALL}"')
-        answer = input('Is this correct? (y/n)')
-        if answer != 'y':
-            return
+# async def main():
+#     try:
+#         channel_name = input(
+#             f"{Fore.CYAN}Please enter a target Telegram channel (e.g., https://t.me/{Fore.LIGHTYELLOW_EX}your_channel{Style.RESET_ALL}):\n")
+#         print(f'You entered "{Fore.LIGHTYELLOW_EX}{channel_name}{Style.RESET_ALL}"')
+#         answer = input('Is this correct? (y/n)')
+#         if answer != 'y':
+#             return
+#
+#         output_directory = sanitizer.sanitize_filename(f"Collection/{channel_name}")
+#         if not os.path.exists(output_directory):
+#             os.makedirs(output_directory)
+#
+#         csv_filename = sanitizer.sanitize_filename(f'{output_directory}/{channel_name}_messages.csv')
+#         print(f'Scraping content from {Fore.LIGHTYELLOW_EX}{channel_name}{Style.RESET_ALL}...')
+#
+#         content = await scrape_channel_content(channel_name)
+#
+#         if content:
+#             df = pd.DataFrame(content, columns=['Datetime', 'Text', 'Username', 'First Name', 'Last Name', 'User ID', 'Views',
+#                                                 'Message URL', 'Media File Path'])
+#             try:
+#                 df.to_csv(csv_filename, index=False)
+#                 print(
+#                     f'Successfully scraped and saved content to {Fore.LIGHTYELLOW_EX}{csv_filename}{Style.RESET_ALL}.')
+#             except Exception as e:
+#                 print(f"An error occurred while saving to CSV: {Fore.RED}{e}{Style.RESET_ALL}")
+#         else:
+#             print(f'{Fore.RED}No content scraped.{Style.RESET_ALL}')
+#
+#     except Exception as e:
+#         print(f"An error occurred: {Fore.RED}{e}{Style.RESET_ALL}")
 
-        output_directory = sanitizer.sanitize_filename(f"Collection/{channel_name}")
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
 
-        csv_filename = sanitizer.sanitize_filename(f'{output_directory}/{channel_name}_messages.csv')
-        print(f'Scraping content from {Fore.LIGHTYELLOW_EX}{channel_name}{Style.RESET_ALL}...')
-
-        content = await scrape_channel_content(channel_name)
-
-        if content:
-            df = pd.DataFrame(content, columns=['Datetime', 'Text', 'Username', 'First Name', 'Last Name', 'User ID', 'Views',
-                                                'Message URL', 'Media File Path'])
-            try:
-                df.to_csv(csv_filename, index=False)
-                print(
-                    f'Successfully scraped and saved content to {Fore.LIGHTYELLOW_EX}{csv_filename}{Style.RESET_ALL}.')
-            except Exception as e:
-                print(f"An error occurred while saving to CSV: {Fore.RED}{e}{Style.RESET_ALL}")
-        else:
-            print(f'{Fore.RED}No content scraped.{Style.RESET_ALL}')
-
-    except Exception as e:
-        print(f"An error occurred: {Fore.RED}{e}{Style.RESET_ALL}")
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+# if __name__ == '__main__':
+#     asyncio.run(main())
--- preprocessor/extractor.py
+++ preprocessor/extractor.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import os
 
 def extract_text_blocks_from_html(html) -> list:
     # HTML 문서에서 텍스트 블록 추출
@@ -11,10 +12,13 @@ def extract_text_blocks_from_html(html) -> list:
 import json
 
 # 텍스트 길이가 가장 길고 특정 텍스트를 포함하는 원소를 반환하는 함수
-def extract_by_length(strings):
-    # json 파일에서 마약 은어/약어 로드
-    # 추후 데이터베이스에서 로드하도록 변경
-    with open("drug_dictionary.json", "r", encoding="utf-8") as filestream:
+def extract_by_length(strings) -> str | None:
+    # json 파일에서 마약 은어/약어 로드(추후 데이터베이스에서 로드하도록 변경)
+    # 모듈 기준으로 drug_dictionary.json 파일 경로 계산
+    current_dir = os.path.dirname(__file__)  # 현재 파일(extractor.py)의 디렉토리
+    dictionary_path = os.path.join(current_dir, "drug_dictionary.json")
+
+    with open(dictionary_path, "r", encoding="utf-8") as filestream:
         dictionary = json.load(filestream)
         
     # 은어/약어 사전에맞는 문자열들을 필터링
@@ -22,18 +26,20 @@ def extract_by_length(strings):
 
     # 조건에 맞는 문자열 중 가장 긴 문자열 반환
     if filtered_strings:
-        return max(filtered_strings, key=len)
+        return str(max(filtered_strings, key=len))
 
     return None
 
+def extract_promotion_content(html: str) -> None | str:
+    return extract_by_length(extract_text_blocks_from_html(html))
 
 
-html_file_path = "sample_webpage.html"
+# html_file_path = "/sample_webpage.html"
+#
+# with open(html_file_path, "r", encoding="utf-8") as file:
+#     sample_html = file.read()  # 파일 내용을 문자열로 읽음
 
-with open(html_file_path, "r", encoding="utf-8") as file:
-    sample_html = file.read()  # 파일 내용을 문자열로 읽음
 
-
-sample_text_blocks = extract_text_blocks_from_html(sample_html)
-print(sample_text_blocks)
-print(extract_by_length(sample_text_blocks))
+# sample_text_blocks = extract_text_blocks_from_html(sample_html)
+# print(sample_text_blocks)
+# print(extract_by_length(sample_text_blocks))
