8033a2758b4fc574afdb72b4102b6749b367ad2c
jungwoohyuk0331 <wjddngur0334@naver.com>
2025-03-23 22:15:52 +0900

    .

--- Modules/clustering/__init__.py
+++ Modules/clustering/__init__.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, request, jsonify
 from . import post, post_similarity
+from .channel import channels, calculate_and_store_similarity
 from .post import dbscan_clustering, collection, db
 
 # Blueprint 설정
 cluster_bp = Blueprint('cluster', __name__, url_prefix='/cluster')
 
 # '/post.py' 엔드포인트
-
 @cluster_bp.route('/post', methods=['POST'])
 def post_clustering():
     data = request.json
@@ -20,11 +20,19 @@ def post_clustering():
         return jsonify({"error": str(e)}), 500
 
 
-# '/get_clustered_results' 엔드포인트
+# '/post_similarity.py' 엔드포인트
 @cluster_bp.route('/post_similarity', methods=['POST'])
 def post_similarity_calculation():
     try:
         result = post_similarity.calculate_and_store_similarity()
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+# '/channel.py' 엔드포인트
+@cluster_bp.route('/channels', methods=['POST'])
+def analyze_channels():
+    try:
+        result = calculate_and_store_similarity(channels)
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
