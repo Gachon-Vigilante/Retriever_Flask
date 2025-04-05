7824bd2fa0311dd2ca63dab820f74f2250250cff
jungwoohyuk0331 <wjddngur0334@naver.com>
2025-04-06 03:23:07 +0900

    api 변경 및 추가

--- clustering/__init__.py
+++ clustering/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request, jsonify
 from . import post, post_similarity
-from .channel import calculate_and_store_similarity
+from .channel import calculate_and_store_channel_similarity
+from .drug_stat import drug_time, drug_type
 from .post import dbscan_clustering, collection, db, perform_clustering_with_cosine
-
+from .post_similarity import post_similarity
 # Blueprint 설정
 cluster_bp = Blueprint('cluster', __name__, url_prefix='/cluster')
 
@@ -27,7 +28,7 @@ def post_clustering():
 @cluster_bp.route('/post_similarity', methods=['POST'])
 def post_similarity_calculation():
     try:
-        result = calculate_and_store_similarity()
+        result = post_similarity()
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -35,11 +36,17 @@ def post_similarity_calculation():
 
 # '/channel.py' 엔드포인트
 @cluster_bp.route('/channels', methods=['POST'])
-def analyze_channels():
-    if not request.is_json:
-        return jsonify({"error": "Request must be in JSON format"}), 400
-    try:
-        result = calculate_and_store_similarity()
-        return jsonify(result), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+def calculate_similarity():
+    result = calculate_and_store_channel_similarity()
+    return jsonify(result), 200
+
+@cluster_bp.route('/drug-type', methods=['GET'])
+def drug_usage_by_type():
+    data = drug_type()
+    return jsonify(data)
+
+@cluster_bp.route('/drug-time', methods=['GET'])
+def drug_usage_over_time():
+    period = request.args.get('period', 'monthly')  # 'weekly' or 'monthly'
+    data = drug_time(period=period)
+    return jsonify(data)
