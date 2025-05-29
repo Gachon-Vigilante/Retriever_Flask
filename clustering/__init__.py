fca77f7264d98a79810f6d612c7a81cdbda266de
jungwoohyuk0331 <wjddngur0334@naver.com>
2025-05-29 20:11:01 +0900

    Update Flask_api_endPoint - 엔드포인트 변경

--- clustering/__init__.py
+++ clustering/__init__.py
@@ -1,32 +1,11 @@
 from flask import Blueprint, jsonify, Response
 from .channel import calculate_and_store_channel_similarity
 from .channel_come_in import calculate_similarity_for_new_channels
-from .post import perform_clustering_with_cosine
-from .post_come_in import clustering_for_new_posts
 from .post_similarity import post_similarity
+from .post import handle_new_posts, maybe_recluster, perform_clustering_with_HDBSCAN
+
 # Blueprint 설정
 cluster_bp = Blueprint('cluster', __name__, url_prefix='/cluster')
-
-# '/post.py' 엔드포인트
-@cluster_bp.route('/post', methods=['POST'])
-# def post_clustering():
-#     try:
-#         result = perform_clustering_with_cosine()
-#         return jsonify(result), 200
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-def post_clustering():
-    try:
-        result = perform_clustering_with_cosine()
-        # 만약 결과가 Flask Response 객체면 (즉, 이미지 등 binary data)
-        if isinstance(result, Response):
-            return result
-        # 아니면 JSON 응답으로 처리
-        else:
-            return jsonify(result), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 
 # '/post_similarity.py' 엔드포인트
 @cluster_bp.route('/post_similarity', methods=['POST'])
@@ -49,9 +28,28 @@ def update_new_channel():
     result = calculate_similarity_for_new_channels()
     return jsonify(result), 200
 
-# '/post_come_in.py' 엔드포인트
+# '/post.py' 엔드포인트
 @cluster_bp.route('/post_update', methods=['POST'])
-def update_new_post():
-    result = clustering_for_new_posts()
+def new_post():
+    result = handle_new_posts()
     return jsonify(result), 200
+
+@cluster_bp.route('/post_recluster', methods=['POST'])
+def recluster_api():
+    result = maybe_recluster(threshold_count=100)
+    return jsonify(result), 200
+
+@cluster_bp.route('/post_cluster', methods=['POST'])
+def post_clustering():
+    try:
+        result = perform_clustering_with_HDBSCAN()
+        # 만약 결과가 Flask Response 객체면 (즉, 이미지 등 binary data)
+        if isinstance(result, Response):
+            return result
+        # 아니면 JSON 응답으로 처리
+        else:
+            return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
